b5dc2b52fa0b049db543f65e6ed4004095099be8
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-08-18 14:50:11 -0700

    Add files via upload

---
+++ Module1_CriticalThinkingPt2/main.cpp
@@ -0,0 +1,32 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 17, 2024
+ *      Author: Megan - School
+ */
+/*
+	Simple Program with a few Errors for Correction
+	Please be sure to correct all outlined errors.
+*/
+
+#include <iostream>
+#include <conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main() {
+
+     //Standard Output Statement
+     cout <<"Welcome to this C++ Program"<< endl;
+
+     cout <<"I have corrected all errors for this program." << endl;
+
+     // Wait For Output Screen
+	_getch();
+
+
+     //Main Function return Statement
+	return 0;
+}
