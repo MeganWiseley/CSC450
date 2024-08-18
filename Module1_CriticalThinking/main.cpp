9952ca7924b761db1f73cea35b0a36a36cdf2148
MeganWiseley <142959058+MeganWiseley@users.noreply.github.com>
2024-08-18 14:49:21 -0700

    Add files via upload

---
+++ Module1_CriticalThinking/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 17, 2024
+ *      Author: Megan - School
+ */
+
+#include <iostream>
+#include <string>
+
+int main() {
+	//Declaring the variables to hold the contact information for Jack Skellington
+	std::string firstName = "Jack";
+	std::string lastName = "Skellington";
+	std::string streetAddress = "121 Pumpkin King Drive";
+	std::string city = "Halloween Town";
+	std::string zipCode = "06071";
+
+	//Print out the information from the above
+	std::cout << "First Name: " << firstName << std::endl;
+	std::cout << "Last Name: " << lastName << std::endl;
+	std::cout << "Street Address: " << streetAddress << std::endl;
+	std::cout << "City: " << city << std::endl;
+	std::cout << "Zip Code: " << zipCode << std::endl;
+
+	return 0;
+}
+
+
+
+
